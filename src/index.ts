5703cf52c0f2b10ae7fc966d1d922f1dfbc30c5c
Ilia Dzhiubanskii <starkyru@gmail.com>
2026-08-10 18:09:17 -0400

    feat: export retry policy and webhook helpers from the package root

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 export { ProdigiClient } from "./client.js";
 export type { ProdigiClientOptions, Environment } from "./client.js";
 export { OrderBuilder } from "./builders/order-builder.js";
-export { ProdigiError, ProdigiApiError } from "./errors.js";
+export {
+  ProdigiError,
+  ProdigiApiError,
+  ProdigiWebhookError,
+} from "./errors.js";
+export { DEFAULT_RETRY_POLICY } from "./retry.js";
+export type { RetryOptions, RetryPolicy } from "./retry.js";
+export { parseCallbackEvent, isCallbackEvent } from "./webhooks.js";
 export * from "./types/index.js";
